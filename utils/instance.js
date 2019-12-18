af455182a85825aca401b2a54e681c2cbb3dc7d8
bot-net <bot-net@bot-net.com>
2019-12-18 11:02:03 +0000

    Using application defined environment variables.

--- utils/instance.js
+++ utils/instance.js
@@ -1,33 +1,38 @@
+const fs = require('fs');
 const express = require('express');
 const Redis = require('ioredis');
 const socketio = require('socket.io');
+const { BRC487 } = require('@s1lv3rsph3r3/central');
+const { sslKey, sslCert } = require(BRC487.commute('environment'));
 
 /* PRODUCTION ONLY */
-// const https = require('https');
+const https = require('https');
 /** **************** */
 
 /* DEVELOPMENT ONLY */
-const http = require('http');
+//const http = require('http');
 /** ***************** */
 
 const app = express();
 
 /* PRODUCTION ONLY */
 // set the ssl cert and key values
-// const server = https.createServer({
-//   key: fs.readFileSync(
-//     /* Use a key generated from .env variables */
-//     /* Replace with generated key */
-//   ),
-//   cert: fs.readFileSync(
-//     /* Use a key generated from .env variables */
-//     /* Replace with generated full certificate chain */
-//   ),
-// }, app);
+const server = https.createServer({
+  key: fs.readFileSync(
+    /* Use a key generated from .env variables */
+    /* Replace with generated key */
+    sslKey
+  ),
+  cert: fs.readFileSync(
+    /* Use a key generated from .env variables */
+    /* Replace with generated full certificate chain */
+    sslCert
+  ),
+}, app);
 /** **************** */
 
 /* DEVELOPMENT ONLY */
-const server = http.createServer(app);
+//const server = http.createServer(app);
 /** ***************** */
 
 const io = socketio(server);
