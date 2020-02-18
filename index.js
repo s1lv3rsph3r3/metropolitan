0bf20ef7b787fd582c164b5fa689902878342354
bot-net <bot-net@bot-net.com>
2020-02-18 13:29:50 +0000

    Remove unused associations to underlying core infrastructure

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /* List the requires based on the core structure */
 
 /* Facades - Returned as the respective proto name */
-const ModuleEventProvider = require('./facade/providers/ModuleEventProviderFacade');
-const ModuleRoutingProvider = require('./facade/providers/ModuleRoutingProviderFacade');
-
 const Event = require('./facade/EventFacade');
 const Route = require('./facade/RouteFacade');
 
@@ -11,15 +8,11 @@ const Route = require('./facade/RouteFacade');
 const Application = require('./utils/Application');
 const generic = require('./utils/generic');
 const instance = require('./utils/instance');
-const SubscriptionFactory = require('./utils/SubscriptionFactory');
 
 module.exports = {
-  ModuleEventProvider,
-  ModuleRoutingProvider,
   Event,
   Route,
   Application,
   generic,
   instance,
-  SubscriptionFactory,
 };
