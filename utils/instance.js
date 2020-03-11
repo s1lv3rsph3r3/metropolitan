b29641a9cc366c93e7d12cb26092bbf484fdb131
bot-net <timothy_a_rawlins@yahoo.com>
2020-03-11 18:58:10 +0000

    Allow application to boot in a non production environment

--- utils/instance.js
+++ utils/instance.js
@@ -3,37 +3,31 @@ const express = require('express');
 const Redis = require('ioredis');
 const socketio = require('socket.io');
 const { BRC487 } = require('@s1lv3rsph3r3/central');
-const { sslKey, sslCert } = require(BRC487.commute('environment'));
-
-/* PRODUCTION ONLY */
-const https = require('https');
-/** **************** */
-
-/* DEVELOPMENT ONLY */
-//const http = require('http');
-/** ***************** */
+const { production, sslKey, sslCert } = require(BRC487.commute('environment'));
 
 const app = express();
+const https = require('https'); // used for production only
+const http = require('http'); // used only for development
+let server;
 
-/* PRODUCTION ONLY */
-// set the ssl cert and key values
-const server = https.createServer({
-  key: fs.readFileSync(
-    /* Use a key generated from .env variables */
-    /* Replace with generated key */
-    sslKey
-  ),
-  cert: fs.readFileSync(
-    /* Use a key generated from .env variables */
-    /* Replace with generated full certificate chain */
-    sslCert
-  ),
-}, app);
-/** **************** */
-
-/* DEVELOPMENT ONLY */
-//const server = http.createServer(app);
-/** ***************** */
+if(production === 'true'){
+  // setup https
+  server = https.createServer({
+    key: fs.readFileSync(
+      // Use a key generated from .env variables 
+      // Replace with generated key
+      sslKey
+    ),
+    cert: fs.readFileSync(
+      // Use a key generated from .env variables
+      // Replace with generated full certificate chain
+      sslCert
+    ),
+  }, app);
+}else{
+  // setup http for local env
+  server = http.createServer(app);
+}
 
 const io = socketio(server);
 const redis = new Redis();
