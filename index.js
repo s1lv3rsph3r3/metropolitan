e31bfff51475309cc18c6e6f7db3b882ca42e217
timothy.rawlins <timothy.rawlins@gentrack.com>
2020-05-11 19:48:04 +0100

    Adjust naming conventions

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const Event = require('./facade/EventFacade');
 const Route = require('./facade/RouteFacade');
 
 /* Utils - Returned as tools to use across the program */
-const Application = require('./utils/Application');
-const generic = require('./utils/generic');
+const applicationUtils = require('./utils/Application');
+const genericUtils = require('./utils/generic');
 // const instance = require('./utils/instance');
 
 module.exports = {
   Event,
   Route,
-  Application,
-  generic,
+  applicationUtils,
+  genericUtils,
   // instance,
 };
